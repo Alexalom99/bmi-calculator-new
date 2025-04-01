7fcc2f21d4d457e665ba7f3d307f0c4ab3abfba5
Alexalom99 <alexalom1999@gmail.com>
2025-04-01 20:35:49 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // ¡Buena suerte!
+
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const button = document.querySelector("#btn");
+const result = document.querySelector("#bmi-result");
+const condition = document.querySelector("#weight-condition");
+
+
+button.addEventListener("click", function () {
+    let total = (weight.value * 10000) / height.value**2;
+    let cond;
+    result.value = total;
+    if (total < 18.5) {
+        cond = "Bajo peso";
+    }else if (total < 24.9){
+        cond = "Peso normal";
+    }else if (total < 29.9) {
+        cond = "Sobrepeso";
+    }
+    else {
+        cond = "Obesidad";
+    }
+    condition.textContent = cond;
+})
